230ffcc86343b88110007e3423b06d19642de305
Ryan Fowler <ryan.fowler19@gmail.com>
2021-08-21 05:40:41 +0000

    Use Go 1.17 to generate protobufs

--- scripts/proto/gen_proto.sh
+++ scripts/proto/gen_proto.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -ex
+#set -ex
 
 if ! docker info &> /dev/null; then
     echo 'Error: Docker is required to be running'
     exit 1
 fi
 
-docker build -f scripts/proto/Dockerfile -t distcache . &> /dev/null
-docker run -it --rm --name=distcache -v "$(pwd)":/go/src/github.com/ryanfowler/distcache distcache protoc -I . --go_out=:grpc/peerpb/v1 --go-grpc_out=grpc/peerpb/v1 grpc/peerpb/v1/peer.proto &> /dev/null
+docker build -f scripts/proto/Dockerfile -t distcache .
+docker run -it --rm --name=distcache -v "$(pwd)":/go/src/github.com/ryanfowler/distcache distcache protoc -I . --go_out=:grpc/peerpb/v1 --go-grpc_out=grpc/peerpb/v1 grpc/peerpb/v1/peer.proto
