ed618a5310d6de48458f2caf38cce833af08a820
Ryan Fowler <ryan.fowler19@gmail.com>
2021-08-21 05:47:44 +0000

    Upgrade buf to v0.52.0

--- scripts/proto/install_buf.sh
+++ scripts/proto/install_buf.sh
@@ -2,7 +2,7 @@
 set -ex
 
 PREFIX="/usr/local" && \
-VERSION="0.32.0" && \
+VERSION="0.52.0" && \
   curl -sSL \
     "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m).tar.gz" | \
     sudo tar -xvzf - -C "${PREFIX}" --strip-components 1
